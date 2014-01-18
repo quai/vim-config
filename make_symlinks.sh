0a06beef3579a84bce39269196f1aa81672e5089
Kay Bærulfsen <berulfsen@gmail.com>
2014-01-18 02:32:23 +0100

    I have to start somewhere

---
+++ make_symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/.vim-config/spf13-vim-3/.vimrc.bundles ~/
+ln -s ~/.vim-config/spf13-vim-3/.vimrc ~/
+ln -s ~/.vim-config/spf13-vim-3/.vim ~/
+ln -s ~/.vim-config/vimrc.local ~/.vimrc.local
