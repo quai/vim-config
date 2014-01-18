fcf4359fdfa1479482c78634f71e7cd19d944960
Kay Bærulfsen <berulfsen@gmail.com>
2014-01-18 03:33:25 +0100

    Added symlink to .vimrc.before

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -3,4 +3,5 @@
 ln -s ~/.vim-config/spf13-vim-3/.vimrc.bundles ~/
 ln -s ~/.vim-config/spf13-vim-3/.vimrc ~/
 ln -s ~/.vim-config/spf13-vim-3/.vim ~/
+ln -s ~/.vim-config/spf13-vim-3/.vimrc.before ~/
 ln -s ~/.vim-config/vimrc.local ~/.vimrc.local
